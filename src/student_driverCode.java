318670c46fbba1b522e2d508810e6c1d22978fff
ebrahimbaig <BAIG.EBRAHIM@GMAIL.COM>
2021-12-07 23:23:21 +0500

    login clean code

--- src/student_driverCode.java
+++ src/student_driverCode.java
@@ -8,89 +8,74 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class student_driverCode {
-            database db;
-            Connection conn;
-            ArrayList<String> profile = new ArrayList<String>();
-   
-  public student_driverCode(){
-        db = new database();
-        conn= db.getConnection();
-  }
-
-//   StudentLoginGUI stdlogin = new StudentLoginGUI();
-     int std_id;
     
-   
-   
-    public void stdLogin(int id, String pass) throws SQLException{
-      
+    int std_id;
+    database db;
+    Connection conn;
+    ArrayList<String> profile = new ArrayList<String>();
 
-       std_id=id; //updating global std_id
-       int check_id; //id from query
-       String Check_pass = null; // pw from query
-       boolean login;
-       
-       try{
- 
-       String query ="select student_id from students where student_id = ?";
-           
-       PreparedStatement st = conn.prepareStatement(query); 
-       st.setInt(1, id);
-//       System.out.println(st);
-       ResultSet rs = st.executeQuery();  
-//           System.out.println(rs);
-       if(!rs.next()){
-           System.out.println("Id Not Found");
-       }
-       
-       else{
-       
-       String query1 ="select student_id, password from students where student_id =?";
-       
-       PreparedStatement st1 = conn.prepareStatement(query1); 
-       st1.setInt(1, id);
-       ResultSet rs1 = st1.executeQuery();  
-//           System.out.println(rs1);
-       while (rs1.next())
-      {
-          System.out.println(rs1.getInt("student_id")+"");
-        check_id = rs.getInt("student_id");   
-        Check_pass =rs1.getString("password");
-      }
-              
-      if (Check_pass.equals(pass)){
-        StdwelcomeGUI a = new StdwelcomeGUI();
-        a.setVisible(true);
-        login = true;
-      }
-      else
-            System.out.println("Password not match"); 
-       }
-       }
-      catch (SQLException ex) {
+    public student_driverCode() {
+        db = new database();
+        conn = db.getConnection();
+    }
+
+
+    
+    public void stdLogin(int id, String pass) throws SQLException {
+
+        std_id = id; //updating global std_id
+        int check_id; //id from query
+        String Check_pass = null; // pw from query
+        boolean login;
+
+        try {
+
+            String query = "select student_id from students where student_id = ?"; // query to check if id exists
+
+            PreparedStatement st = conn.prepareStatement(query); //creating and preparing statements
+            st.setInt(1, id); //passing id as parameter
+            ResultSet rs = st.executeQuery();
+            
+            if (!rs.next()) {
+                System.out.println("Id Not Found");
+            } else {
+
+                String query1 = "select student_id, password from students where student_id =?";
+
+                PreparedStatement st1 = conn.prepareStatement(query1);
+                st1.setInt(1, id);
+                ResultSet rs1 = st1.executeQuery();
+
+                while (rs1.next()) {
+                    System.out.println(rs1.getInt("student_id") + "");
+                    check_id = rs.getInt("student_id");
+                    Check_pass = rs1.getString("password");
+                }
+
+                if (Check_pass.equals(pass)) {
+                    StdwelcomeGUI a = new StdwelcomeGUI();
+                    a.setVisible(true);
+                    login = true;
+                } else {
+                    System.out.println("Password not match");
+                }
+            }
+        } catch (SQLException ex) {
             Logger.getLogger(student_driverCode.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-    
-    public void showProfile() throws SQLException{
-        
-       String query ="select * from students where student_id =1";
-       PreparedStatement st = conn.prepareStatement(query); 
-       ResultSet rs = st.executeQuery(query);
-       while (rs.next())
-      {
-        profile.add(rs.getString("student_id"));
-      }
-        
-    }
-    
-    
-    
-    
-}
-       
-        
 
+    public void showProfile() throws SQLException {
+
+        String query = "select * from students where student_id =1";
+        PreparedStatement st = conn.prepareStatement(query);
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            profile.add(rs.getString("student_id"));
+        }
+
+    }
+
+}
